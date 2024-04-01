0d5e81bbce38867f0e543a10f89bac1d219c929b
AndrewKryvenko <124074819+AndrewKryvenko@users.noreply.github.com>
2024-04-01 16:47:56 +0300

    main.py

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
 import asyncio
 import json
-import aiogram
-from aiogram import types
-from aiogram import Bot, Dispatcher
-from aiogram.fsm.context import FSMContext
-from aiogram.filters.command import Command
+from aiogram import Bot, Dispatcher, types
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
 
 bot = Bot(token="6442089419:AAG4q9RlLlpJ7w4HEKusOqTXUA18MSMaK_w")
 dp = Dispatcher(bot)
 
-
-@dp.message_handler(Command('start'))
-async def start(message: types.Message, state: FSMContext):
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
     item1 = KeyboardButton(text="Вибрати товар", web_app=WebAppInfo(url='https://andrewkryvenko.github.io/dyplom2.github.io//'))
     keyboard = ReplyKeyboardMarkup(keyboard=[[item1]], resize_keyboard=True)
-    await bot.send_message(message.from_user.id,"Вітаємо! 🙌🏼\n Натисність на кнопку МЕНЮ знизу зліва щоб замовити їжу👇🏼", reply_markup=keyboard, parse_mode="Markdown")
-
+    await message.answer("Вітаємо! 🙌🏼\n Натисність на кнопку МЕНЮ знизу зліва щоб замовити їжу👇🏼", reply_markup=keyboard, parse_mode="Markdown")
 
 @dp.message_handler(content_types=types.ContentType.TEXT)
 async def web_app(message: types.Message):
@@ -31,19 +25,14 @@ async def web_app(message: types.Message):
 
     message_text += f"Загальна вартість: {parsed_data['totalPrice']}"
 
-    await bot.send_message(callback_query.from_user.id, f"""
-{message}
-""")
+    await bot.send_message(message.from_user.id, message_text)
     await bot.send_message('-1002022582711', f"""
 Нове замовлення\n
 {message_text}
 """)
 
-
 async def main():
-        await dp.start_polling(bot)
-
-
+    await dp.start_polling()
 
 if __name__ == "__main__":
     asyncio.run(main())
